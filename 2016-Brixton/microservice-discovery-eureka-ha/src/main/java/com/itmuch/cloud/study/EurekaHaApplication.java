f2ffbdc7e6203245ea8771dff29a7d003aa69c72
Peng.lv <LP920lp821!>
2020-03-10 16:48:31 +0800

    可运行

---
+++ 2016-Brixton/microservice-discovery-eureka-ha/src/main/java/com/itmuch/cloud/study/EurekaHaApplication.java
@@ -0,0 +1,18 @@
+package com.itmuch.cloud.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 使用Eureka做服务发现.
+ *
+ * @author eacdy
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaHaApplication{
+public static void main(String[]args){
+        SpringApplication.run(EurekaHaApplication.class,args);
+        }
+        }
